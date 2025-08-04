78acf29a03778d0fb389bafb892f81d939c5da60
typovrak <typovrak@gmail.com>
2025-08-04 22:17:46 +0200

    understand blocks, extends, includes and tags

--- django/app/mysite/views.py
+++ django/app/mysite/views.py
@@ -5,4 +5,17 @@ from django.template import loader
 # @require_http_methods(["GET"])
 def index(request):
     template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    context = {
+        "blog_entries": [
+            {
+                "title": "test1",
+                "body": "body1",
+            },
+            {
+                "title": "test2",
+                "body": "body2",
+            },
+        ]
+    }
+
+    return HttpResponse(template.render(context, request))
