efdbca6598264eba4fde74c62ef979d2ba762f1b
typovrak <typovrak@gmail.com>
2025-08-04 23:52:00 +0200

    update

--- django/app/core/templatetags/mytags.py
+++ django/app/core/templatetags/mytags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template import Node
 
 register = template.Library()
 
@@ -11,3 +12,19 @@ def hello(name):
 @register.filter
 def upperfirst(value):
     return value[0].upper() + value[1:]
+
+
+class UpperNode(Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        content = self.nodelist.render(context)
+        return content.upper()
+
+
+@register.tag(name="upper")
+def do_upper(parser, token):
+    nodelist = parser.parse(("endupper",))
+    parser.delete_first_token()
+    return UpperNode(nodelist)
