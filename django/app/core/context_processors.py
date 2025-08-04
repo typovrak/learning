50dfe1b14320d440630dbf07c98a33c2a635d2f9
typovrak <typovrak@gmail.com>
2025-08-04 23:12:06 +0200

    add globals variables to every template

---
+++ django/app/core/context_processors.py
@@ -0,0 +1,10 @@
+def globals(request):
+    return {
+        "globals": {
+            "site_name": "Typovrak",
+            "social_links": {
+                "github": "https://github.com/typovrak",
+                "malt": "https://www.malt.fr/profile/morganscholz",
+            },
+        },
+    }
